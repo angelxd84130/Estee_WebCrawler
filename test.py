6245e1ab36d0dc2fb7e7ddd72349644513502c26
angel <angelxd84130@gmail.com>
2022-09-20 07:37:59 +0800

    add analysis

--- test.py
+++ test.py
@@ -1,28 +1,28 @@
-import os, json
+import plotly
 import pandas as pd
-def getFilePass() -> list[str]:
-    json_files = [pos_json for pos_json in os.listdir() if pos_json.endswith('.json')]
-    return json_files
+import openpyxl
 
-def fileMapping():
-    jFiles = getFilePass()
-    jsonList = []
-    for j in jFiles:
-        try:
-             with open(j, 'r+', encoding='utf8') as f:
-                content = json.load(f)
-                f.close()
-        except:
-            content = []
-        jsonList += content
-        print("add file:", j,  "len:", len(jsonList))
+def daily_discount_rank(df) -> None:
+    #data = df['所有價格'].apply(pd.Series)
+    data = pd.concat([df.drop(['所有價格'], axis=1), df['所有價格'].apply(pd.Series)], axis=1)
+    #data = data[['產品編號', '產品名', '資料更新時間', '市售價', '折扣後價格']]
+    data['DateTime'] = data['資料更新時間'].str[:10]
+    data['Discount'] = (data['市售價'] - data['折扣後價格'])
+    data = data[['產品編號', '產品名', 'DateTime', 'Discount']]
+    data.sort_values(by=['DateTime', '產品編號', 'Discount'], inplace=True)
+    data.dropna(inplace=True)
+    data.drop_duplicates(['產品編號', '產品名', 'DateTime', 'Discount'], keep='last', inplace=True, ignore_index=True)
+    data.to_excel('daily_discount_rank.xlsx', index=False)
 
-        with open("data.json", "w+", encoding='utf8') as f:
-            f.write(json.dumps(jsonList, indent=4, ensure_ascii=False))
-            f.close()
-        print("finish mapping")
+
+def weekly_price_change(df) -> None:
+    data = pd.concat([df.drop(['所有價格'], axis=1), df['所有價格'].apply(pd.Series)], axis=1)
 
 
 
+def Analysis() -> None:
+    df = pd.read_json('data.json')
+    daily_discount_rank(df)
+    weekly_price_change()
 
-fileMapping()
+Analysis()
