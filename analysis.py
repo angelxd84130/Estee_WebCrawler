591096bd791764118f123bd0a95f20ded3503613
angel <angelxd84130@gmail.com>
2022-09-21 01:34:05 +0800

    modify daily analysis

--- analysis.py
+++ analysis.py
@@ -3,13 +3,13 @@ from datetime import timedelta
 import openpyxl
 
 def daily_discount_rank(df) -> None:
-    #data = df['所有價格'].apply(pd.Series)
+    # expand all price
     data = pd.concat([df.drop(['所有價格'], axis=1), df['所有價格'].apply(pd.Series)], axis=1)
-    #data = data[['產品編號', '產品名', '資料更新時間', '市售價', '折扣後價格']]
     data['DateTime'] = data['資料更新時間'].str[:10]
     data['Discount'] = (data['市售價'] - data['折扣後價格'])
     data = data[['產品編號', '產品名', 'DateTime', 'Discount']]
-    data.sort_values(by=['DateTime', '產品編號', 'Discount'], inplace=True)
+    data["Rank"] = data.groupby("DateTime")["Discount"].rank("dense", ascending=False)
+    data.sort_values(by=['DateTime', 'Rank',  '產品編號'], inplace=True, ascending=[True, True, True])
     data.dropna(inplace=True)
     data.drop_duplicates(['產品編號', '產品名', 'DateTime', 'Discount'], keep='last', inplace=True, ignore_index=True)
     data.to_excel('daily_discount_rank.xlsx', index=False)
@@ -35,6 +35,6 @@ def weekly_price_change(df) -> None:
 def Analysis() -> None:
     df = pd.read_json('data.json')
     daily_discount_rank(df)
-    weekly_price_change(df)
+    #weekly_price_change(df)
 
 Analysis()
